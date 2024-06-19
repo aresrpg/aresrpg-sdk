8e5b661804647c40332e3f0ed6fcbccf40577bac
Sceat <fetch@sceat.xyz>
2024-06-20 02:25:38 +0900

    chore: types

--- types/sui.d.ts
+++ types/sui.d.ts
@@ -288,7 +288,7 @@ export function SDK({ rpc_url, wss_url, network, websocket_constructor, allow_fa
         bytes: any;
         signature: any;
         sender: any;
-    }): Promise<import("@mysten/sui/graphql").GraphQLQueryResult<Record<string, unknown>>>;
+    }): Promise<boolean>;
     /** @return {Promise<bigint>} balance */
     get_sui_balance(owner: any): Promise<bigint>;
     subscribe(on_message: any): Promise<import("@mysten/sui/client").Unsubscribe>;
