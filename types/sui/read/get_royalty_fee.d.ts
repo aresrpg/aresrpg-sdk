45e7c5339b2534d187f6b61d1f7c28df4de3e502
sohkey <sohkey.fetch@gmail.com>
2024-07-04 12:04:46 +0200

    feat: add method get_royalty_fee
    update: add try catch

---
+++ types/sui/read/get_royalty_fee.d.ts
@@ -0,0 +1,2 @@
+/** @param {import("../../../types.js").Context} context */
+export function get_royalty_fee({ sui_client, kiosk_client }: import("../../../types.js").Context): (item_type: any) => Promise<any>;
