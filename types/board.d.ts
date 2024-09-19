05ba5de7c5d7550197e98718183d94408d248ea1
Sceat <fetch@sceat.xyz>
2024-09-19 12:41:00 +0800

    feat: lint generated types & naming

--- types/board.d.ts
+++ types/board.d.ts
@@ -1,33 +1,29 @@
-export type Vec2 = {
-    x: number;
-    z: number;
+export function iter_board_data(board: any): Generator<{
+    index: number;
+    pos: {
+        x: any;
+        y: any;
+    };
+    data: any;
+}, void, unknown>;
+export function format_board_data(board_element: any): {
+    materialId: any;
+    type: any;
 };
-
-export type Vec3 = {
+export function extract_border_blocks(board: any): {
+    index: number;
+    pos: {
+        x: any;
+        y: any;
+    };
+    data: any;
+}[];
+export function find_split_axis(board: any): {
     x: number;
     y: number;
-    z: number;
 };
-
-export enum BlockCategory {
-    FLAT = 0,
-    HOLE = 1,
-    OBSTACLE = 2,
-}
-
-export type BlockType = number
-
-export type BoardBlock = {
-    type: BlockType
-    category: BlockCategory
-}
-
-export type BoardData = {
-    origin: Vec3
-    size: Vec2
-    data: BoardBlock[]
-}
-
-export * as BoardUtils from "../src/board_utils"
-
-export function splitBoard(board: BoardData)
+export function sort_by_side(input_blocks: any, board: any): {
+    first: any[];
+    second: any[];
+};
+export function random_select_items(items: any, count: any): any[];
--- types/sui/read/get_character_by_id.d.ts
+++
@@ -1,2 +0,0 @@
-/** @param {import("../../../types.js").Context} context */
-export function get_character_by_id(context: import("../../../types.js").Context): (id: any) => Promise<import("../../../types.js").SuiCharacter>;
