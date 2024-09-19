b7861afdc45c0e04cf9ec42f57b4f6221e55b340
Sceat <fetch@sceat.xyz>
2024-09-19 12:41:00 +0800

    feat: lint generated types & naming

--- types/sui/read/get_aresrpg_kiosk.d.ts
+++
@@ -1,2 +0,0 @@
-/** @param {import("../../../types.js").Context} context */
-export function get_aresrpg_kiosk({ kiosk_client }: import("../../../types.js").Context): (address: any) => Promise<import("@mysten/kiosk").KioskOwnerCap>;
---
+++ types/utils/math.d.ts
@@ -0,0 +1,6 @@
+export function vec2_sub(v1: any, v2: any): {
+    x: number;
+    y: number;
+};
+export function vec2_dist(v1: any, v2: any): number;
+export function vec2_dot(v1: any, v2: any): number;
