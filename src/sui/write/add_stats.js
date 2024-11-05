e1090e9255b65548ed01aacc780db415a75f36f4
Sceat <fetch@sceat.xyz>
2024-11-05 19:39:57 +0800

    feat: add reset stats function

--- src/sui/write/add_stats.js
+++ src/sui/write/add_stats.js
@@ -22,3 +22,30 @@ export function add_stats({ types }) {
     })
   }
 }
+
+/** @param {import("../../../types.js").Context} context */
+export function reset_character_stats({ types }) {
+  return ({
+    tx = new Transaction(),
+    item_kiosk,
+    item_kiosk_cap,
+    character_kiosk,
+    character_kiosk_cap,
+    character_id,
+    item_id,
+  }) => {
+    tx.moveCall({
+      target: `${types.LATEST_PACKAGE_ID}::character_stats::reset_character_stats`,
+      arguments: [
+        tx.object(item_kiosk),
+        item_kiosk_cap,
+        tx.object(character_kiosk),
+        character_kiosk_cap,
+        tx.pure.id(character_id),
+        tx.pure.id(item_id),
+        tx.object(types.ITEM_PROTECTED_POLICY),
+        tx.object(types.VERSION),
+      ],
+    })
+  }
+}
--- types/sui/write/add_stats.d.ts
+++ types/sui/write/add_stats.d.ts
@@ -7,4 +7,14 @@ export function add_stats({ types }: import("../../../types.js").Context): ({ tx
     stat: any;
     amount: any;
 }) => void;
+/** @param {import("../../../types.js").Context} context */
+export function reset_character_stats({ types }: import("../../../types.js").Context): ({ tx, item_kiosk, item_kiosk_cap, character_kiosk, character_kiosk_cap, character_id, item_id, }: {
+    tx?: Transaction;
+    item_kiosk: any;
+    item_kiosk_cap: any;
+    character_kiosk: any;
+    character_kiosk_cap: any;
+    character_id: any;
+    item_id: any;
+}) => void;
 import { Transaction } from '@mysten/sui/transactions';
