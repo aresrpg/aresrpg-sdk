1bbcc7fe39e419737f2370f41d636b629d7d1288
etienne <em85@pm.me>
2024-11-11 23:22:51 +0000

    fix: handle bad input for random select + misc refactor

--- src/board.js
+++ src/board.js
@@ -1,13 +1,13 @@
 import { vec2_dist, vec2_dot, vec2_sub } from './utils/math.js'
 
-const get_board_bounds = board => {
-  const origin = { x: board.origin.x, y: board.origin.z }
-  const end = { x: origin.x + board.size.x, y: origin.y + board.size.z }
-  const center = {
-    x: origin.x + board.size.x / 2,
-    y: origin.y + board.size.z / 2,
+const get_board_center = board_bounds => {
+  const { min: origin, max: end } = board_bounds
+  const board_size = vec2_sub(end, origin)
+  const board_center = {
+    x: origin.x + board_size.x / 2,
+    y: origin.y + board_size.y / 2,
   }
-  return { origin, end, center }
+  return board_center
 }
 
 export const format_board_data = board_element => ({
@@ -16,7 +16,7 @@ export const format_board_data = board_element => ({
 })
 
 export function* iter_board_data(board) {
-  const { origin, end } = get_board_bounds(board)
+  const { min: origin, max: end } = board.bounds
   let index = 0
 
   for (let { y } = origin; y < end.y; y++) {
@@ -37,14 +37,16 @@ export function* iter_board_data(board) {
 }
 
 export const extract_border_blocks = board => {
+  const { min: origin, max: end } = board.bounds
+  const board_size = vec2_sub(end, origin)
   const border_blocks = []
   const blocks = iter_board_data(board)
   const is_edge_block = block_index => {
     const neighbours = [
       block_index - 1,
       block_index + 1,
-      block_index + board.size.x,
-      block_index - board.size.x,
+      block_index + board_size.x,
+      block_index - board_size.x,
     ].map(index => board.data[index]?.type)
     return neighbours.filter(val => val).length < neighbours.length
   }
@@ -59,19 +61,19 @@ export const extract_border_blocks = board => {
 
 export const find_split_axis = board => {
   // find nearest edge block from board center
-  const { center, end } = get_board_bounds(board)
-  let nearest_edge = end
+  const board_center = get_board_center(board.bounds)
+  let nearest_edge = board.bounds.max
 
   const edge_blocks = extract_border_blocks(board)
   for (const block of edge_blocks) {
     nearest_edge =
-      vec2_dist(block.pos, center) < vec2_dist(nearest_edge, center)
+      vec2_dist(block.pos, board_center) < vec2_dist(nearest_edge, board_center)
         ? block.pos
         : nearest_edge
   }
 
   // compute board dividing axis
-  const split_axis = vec2_sub(nearest_edge, center)
+  const split_axis = vec2_sub(nearest_edge, board_center)
   return split_axis
 }
 
@@ -80,11 +82,11 @@ export const sort_by_side = (input_blocks, board) => {
     first: [],
     second: [],
   }
-  const { center } = get_board_bounds(board)
+  const board_center = get_board_center(board.bounds)
   const split_axis = find_split_axis(board)
   const project_axis = { x: split_axis.y, y: -split_axis.x }
   const is_first_side = pos => {
-    const diff = vec2_sub(pos, center)
+    const diff = vec2_sub(pos, board_center)
     const projection = vec2_dot(diff, project_axis)
     return projection < 0
   }
@@ -101,14 +103,23 @@ export const sort_by_side = (input_blocks, board) => {
 
 export const random_select_items = (items, count) => {
   const selected = []
-  while (selected.length < count) {
-    const item_index = Math.round(Math.random() * (items.length - 1))
-    const item = items[item_index]
-    if (item) {
-      selected.push(item)
-    } else {
-      console.warn(`unexpected, selected index: ${item_index}, items: `, items)
+  if (items.length > count) {
+    while (selected.length < count) {
+      const item_index = Math.round(Math.random() * (items.length - 1))
+      const item = items[item_index]
+      if (item) {
+        selected.push(item)
+      } else {
+        console.warn(
+          `unexpected, selected index: ${item_index}, items: `,
+          items,
+        )
+      }
     }
+  } else {
+    console.warn(
+      `bad input: argument count ${count} must be higher than provided items length ${items.length} `,
+    )
   }
   return selected
 }
